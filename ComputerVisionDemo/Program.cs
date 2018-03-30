2c8b758a7ce6ac76cb926b9d83ba2ab2afc6dc4f
Fabián Varela Bonett <jfabian.303@gmail.com>
2018-03-29 21:48:07 -0500

    Create project ComputerVision

---
+++ ComputerVisionDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerVisionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
